d92935fc537da61b2a50091435847aa82508d8bf
Pedro Henrique Moreira Caixeta Ferreira <phmcf13@gmail.com>
2023-09-16 19:02:28 -0300

    Update Aluguel.java

--- codigo/Lab/ALUGUELEQUIPAMENTOS/src/main/java/aluguelequipamentos/Aluguel.java
+++ codigo/Lab/ALUGUELEQUIPAMENTOS/src/main/java/aluguelequipamentos/Aluguel.java
@@ -50,3 +50,17 @@ public class Aluguel {
     public Date getDataFim() {
         return dataFim;
     }
+
+     public double calcularValorTotal() {
+        long diffInDays = (dataFim.getTime() - dataInicio.getTime()) / (1000 * 60 * 60 * 24);
+        return diffInDays * equipamento.getValorDiario();
+    }
+
+    @Override
+    public String toString() {
+        SimpleDateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy");
+        return "Equipamento: " + equipamento.getDescricao() + ", Data Início: " + dateFormat.format(dataInicio)
+                + ", Data Fim: " + dateFormat.format(dataFim) + ", Valor Total: " + calcularValorTotal();
+    }
+
+}
