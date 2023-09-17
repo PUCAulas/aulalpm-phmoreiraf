01c1641a422a41da996aabb729b2cc0b0e64ed1a
Pedro Henrique Moreira Caixeta Ferreira <phmcf13@gmail.com>
2023-09-17 20:15:12 -0300

    Update Main.java

--- codigo/Lab/ALUGUELEQUIPAMENTOS/src/main/java/aluguelequipamentos/Main.java
+++ codigo/Lab/ALUGUELEQUIPAMENTOS/src/main/java/aluguelequipamentos/Main.java
@@ -9,75 +9,80 @@ import java.util.List;
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) throws ParseException {
-        Scanner scanner = new Scanner(System.in);
-        //Cliente clienteNovo = null;
-        // String nomeCliente;
-        // List<Cliente> clienteEntrar = null;
-        // Cliente clienteNovo = null;
-        // int clienteExistente = 0;
+    Scanner scanner = new Scanner(System.in);
         int idCliente = 0;
-        int idClienteAtual = 0;
-        Cliente cliente = null;
-        Aluguel aluguel = null;
+        //int idClienteAtual = 0;
+        Cliente novoCliente = null;
+        // Aluguel aluguel = null;
+        List<Cliente> clientesCad = new ArrayList<>();
         int escolha = -1;
 
         SimpleDateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy");
 
-        System.out.println("Escolha uma opcao");
+        System.out.println("Escolha uma opção");
         System.out.println("1- Registrar Cliente");
         System.out.println("2- Registrar Aluguel");
-        System.out.println("3- Consulta de aluguéis passados");
-        System.out.println("4- Consulta de aluguéis atuais");
+        System.out.println("3- Consulta de alugueis passados");
+        System.out.println("4- Consulta de alugueis atuais");
         System.out.println("5- Relatório mensal");
         System.out.println("0- Sair");
 
-        escolha = scanner.nextInt();
-
         while (escolha != 0) {
+            escolha = scanner.nextInt();
+            scanner.nextLine();
             switch (escolha) {
                 case 1:
-
                     System.out.println("Digite o nome do cliente:");
                     scanner.nextLine(); // Limpar a quebra de linha pendente
                     String nomeCliente = scanner.nextLine();
-                    cliente = new Cliente(nomeCliente);
+                    novoCliente = new Cliente(nomeCliente);
+                    clientesCad.add(novoCliente); // Adiciona o cliente à lista de clientes cadastrados
                     System.out.println("Cliente registrado com sucesso: " + nomeCliente);
                     break;
-
                 case 2:
-                     if (cliente == null) {
-                        System.out.println("Por favor, registre um cliente primeiro.");
-                        break;
+
+                    System.out.println("Clientes disponíveis para aluguel:");
+                    for (Cliente cliente : clientesCad) {
+                        System.out.println("ID: " + cliente.getID() + ", Nome: " + cliente.getNome());
                     }
 
-                    System.out.println("Digite a data de início (dd/MM/yyyy):");
-                    Date dataInicio = dateFormat.parse(scanner.next());
+                    System.out.println("Digite o ID do cliente:");
+                    idCliente = scanner.nextInt();
+                    scanner.nextLine();
 
-                    System.out.println("Digite a data de término (dd/MM/yyyy):");
-                    Date dataFim = dateFormat.parse(scanner.next());
+                    Cliente clienteSelecionado = null;
+                    for (Cliente cliente : clientesCad) {
+                        if (cliente.getID() == idCliente) {
+                            clienteSelecionado = cliente;
+                            break;
+                        }
+                    }
 
-                    System.out.println("Digite o código do equipamento:");
-                    int codigoEquipamento = scanner.nextInt();
+                    if (clienteSelecionado != null) {
+                        System.out.println("Digite a data de início (dd/MM/yyyy):");
+                        Date dataInicio = dateFormat.parse(scanner.next());
 
-                    System.out.println("Digite a descrição do equipamento:");
-                    scanner.nextLine(); // Limpar a quebra de linha pendente
-                    String descricaoEquipamento = scanner.nextLine();
+                        System.out.println("Digite a data de término (dd/MM/yyyy):");
+                        Date dataFim = dateFormat.parse(scanner.next());
 
-                    System.out.println("Digite o valor diário do equipamento:");
-                    double valorDiarioEquipamento = scanner.nextDouble();
+                        System.out.println("Digite o código do equipamento:");
+                        int codigoEquipamento = scanner.nextInt();
 
-                    Equipamento equipamento = new Equipamento(codigoEquipamento, descricaoEquipamento,
-                            valorDiarioEquipamento);
+                        System.out.println("Digite a descrição do equipamento:");
+                        scanner.nextLine(); // Limpar a quebra de linha pendente
+                        String descricaoEquipamento = scanner.nextLine();
 
-                    aluguel = cliente.registrarAluguel(cliente, equipamento, dataInicio, dataFim);
+                        System.out.println("Digite o valor diário do equipamento:");
+                        double valorDiarioEquipamento = scanner.nextDouble();
 
-                    if (aluguel != null) {
-                        System.out.println("Aluguel registrado com sucesso.");
-                        System.out.println("Valor total do aluguel: " + aluguel.calcularValorTotal());
+                        Equipamento equipamento = new Equipamento(codigoEquipamento, descricaoEquipamento,
+                                valorDiarioEquipamento);
+
+                        clienteSelecionado.registrarAluguel(equipamento, dataInicio, dataFim);
                     } else {
-                        System.out.println("Erro ao registrar o aluguel.");
+                        System.out.println("Cliente não encontrado.");
                     }
+
                     break;
                 case 3:
 
